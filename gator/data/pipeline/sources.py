4ecea8b0fa5e2653c81433a85b2f27cb4c2635fa
galacticglum <nerftactic09@gmail.com>
2022-05-29 09:40:13 -0400

    feat(data-pipeline): add custom kwargs to requests.get call

--- gator/data/pipeline/sources.py
+++ gator/data/pipeline/sources.py
@@ -1,4 +1,5 @@
 """Built-in dataset sources."""
+from typing import Any
 from pathlib import Path
 from abc import ABC, abstractmethod
 
@@ -49,20 +50,31 @@ class LocalFileDataSource(DataSource):
 
 
 class RemoteFileDataSource(DataSource):
+<<<<<<< Updated upstream
     """A data source that reads a file from the web.
     """
     def __init__(self, url: str):
+=======
+    """A data source that reads a file from the web using a GET request."""
+    
+    def __init__(self, url: str, req_kwargs: Any = None):
+>>>>>>> Stashed changes
         """Create a new RemoteFileDataSource.
 
         Args:
             url: The URL of the file to read.
+<<<<<<< Updated upstream
             
+=======
+            req_kwargs: Keyword arguments to pass to the requests.get() call.
+>>>>>>> Stashed changes
         """
         self._url = url
+        self._req_kwargs = req_kwargs
 
     def collect(self) -> bytes:
         """Download the file from the web. Return the contents of the file as
         a bytes object.
         """
-        with requests.get(self._url) as r:
+        with requests.get(self._url, **self._req_kwargs) as r:
             return r.content
