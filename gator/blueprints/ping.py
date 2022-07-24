04eb210cfbdc164368d151a58add36b6105c06b3
galacticglum <galacticglum@users.noreply.github.com>
2022-07-24 16:21:59 +0000

    autopep8 action fixes

--- gator/blueprints/ping.py
+++ gator/blueprints/ping.py
@@ -9,10 +9,9 @@ bp = Blueprint('ping', __name__, url_prefix='/ping')
 @bp.get('/')
 def index():
     """"An endpoint for checking if the Gator service is up and running.
-    
+
     It does not report the health of the service, just whether it is
     reachable (i.e. running).  The return value of this endpoint can be
     anything as long as the response has a status code of 200.
     """
     return 'pong'
-    
